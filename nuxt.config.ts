2732962c89e14be338c217db8d31cdee267fa5f6
Cody Wakeford <codypwakeford@gmail.com>
2024-10-25 22:01:17 +0000

    Added layers preview

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
+	compatibilityDate: "2024-04-03",
+	devtools: { enabled: true },
+	hooks: {
+		ready: (nuxt) => {
+			const layers = nuxt.options._layers || []
+			console.log(
+				"Nuxt Layers:",
+				layers.map((layer) => layer.config.rootDir),
+			)
+		},
+	},
 })
